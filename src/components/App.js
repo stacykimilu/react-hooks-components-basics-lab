de1eaf72e87121f98019d495d0e121767003b529
Stacy <stacykimilu@gmail.com>
2024-04-25 14:31:06 +0300

    components basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
@@ -16,15 +16,20 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
